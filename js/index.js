bffa4edae41047737049f8d1b04795d54a1af0d2
sobaint01 <walebellomail@gmail.com>
2022-02-19 03:33:52 -0500

    icon js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$("#searchIcon").hover ( function(){
+    $(".searchform").css("display","flex" );
+    
+  });
+  
+  $("#formCloser").onClick ( function(){
+    $("searchform").css("display","none" );
+    
+  });
